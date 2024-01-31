f457d172d728c9926d064bafa2fa3ec455247bc2
anilmhrzn <anilmaharjan995@gmail.com>
2024-01-31 22:52:38 +0545

    about private folders

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SecreteOrPrivate = () => {
+  return (
+    <div>SecreteOrPrivate</div>
+  )
+}
+
+export default SecreteOrPrivate
+
+//why private folders???
+// private folders in next js basically is used for 
+// -separating UI logic from routing logic 
+// -consistently organizinf internal files across a project
+// -sorting and frouping files in code editors 
+
+
+
+//use of %5f is can be used as prefix if we want to use the _ (underscore) in  folder name
